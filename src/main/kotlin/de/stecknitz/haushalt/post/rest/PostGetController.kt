2f02cef233f8d21d0db5019e2de9831b3e00d1c1
Alexander Stecknitz <Alex.Stecknitz@web.de>
2022-06-29 16:34:19 +0200

    feat: added return value at findAll

--- src/main/kotlin/de/stecknitz/haushalt/post/rest/PostGetController.kt
+++ src/main/kotlin/de/stecknitz/haushalt/post/rest/PostGetController.kt
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.http.ResponseEntity
+import org.springframework.http.ResponseEntity.ok
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -18,8 +19,8 @@ class PostGetController(val readService: PostReadService) {
     @GetMapping(produces = [APPLICATION_JSON_VALUE])
     suspend fun findAll():  ResponseEntity<List<Post>>? {
         logger.debug("FindAll: ")
-        //return readService.findAll()
-        return null
+        val posts = readService.findAll()
+        return ok(posts)
     }
 
     companion object {
