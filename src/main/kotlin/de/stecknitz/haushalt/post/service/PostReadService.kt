1bede10192f73ddf211a23b1aaf80748d6dd76c1
Alexander Stecknitz <Alex.Stecknitz@web.de>
2022-06-29 16:34:04 +0200

    feat: added findAll in PostReadService

--- src/main/kotlin/de/stecknitz/haushalt/post/service/PostReadService.kt
+++ src/main/kotlin/de/stecknitz/haushalt/post/service/PostReadService.kt
@@ -1,7 +1,28 @@
 package de.stecknitz.haushalt.post.service
 
+import de.stecknitz.haushalt.post.entity.Post
 import org.springframework.stereotype.Service
 
 @Service
 class PostReadService {
+
+    suspend fun findAll(): List<Post> = listOf(
+            Post(
+                id = 1,
+                beschreibung = "FindAll-Mock-1",
+            ),
+            Post(
+                id = 2,
+                beschreibung = "FindAll-Mock-2",
+            ),
+            Post(
+                id = 3,
+                beschreibung = "FindAll-Mock-3",
+            ),
+            Post(
+                id = 4,
+                beschreibung = "FindAll-Mock-4",
+            ),
+    )
+
 }
